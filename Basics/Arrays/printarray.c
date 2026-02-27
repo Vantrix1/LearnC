e36ed569228d9037de564f3b7ae2c0d6a635fe4f
v4 <111375797+Vantrix1@users.noreply.github.com>
2026-02-27 14:23:47 +0000

    Update printarray.c

--- Basics/Arrays/printarray.c
+++ Basics/Arrays/printarray.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
-int myarray[] = {1,5,10,24,48,98,244,8372}
+int myarray[] = {1,5,10,24,48,98,244,8372};
 
-printf("%d", myarray[0]) // Prints first number in array using index 0
+printf("%d", myarray[0]); // Prints first number in array using index 0
+return 0;
